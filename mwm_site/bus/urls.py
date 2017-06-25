04a2857867bdf7c4db465b6fd606a76b10253198
Daniel Jordan <daniel.jordan1@ucdconnect.ie>
2017-06-25 19:42:45 +0100

    Hooked up forms to pickled sklearn model. Can use parameters to make predictions.

--- mwm_site/bus/urls.py
+++ mwm_site/bus/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^var/(?P<var>[\w-]+)/$', views.var_input, name='var_input'),
 ]
--- mwm_site/bus/views.py
+++ mwm_site/bus/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .scripts import predict, convert_weekday
+import datetime
 
 
 def index(request):
-    context = {"hour": "6 hours", }
+    pred = 0
+
+    if request.GET:
+        day = convert_weekday(request.GET["day"])
+        hour = int(request.GET["hour"])
+        stop = int(request.GET["stop"])
+        pred = predict(stop, hour, day)
+        pred = str(datetime.timedelta(seconds=pred))
+        # pred = return_number(stop, hour, day)
+
+        print("Variables")
+        print(request.GET)
+        print("\n\n")
+
+
+    else:
+        print("None!\n\n")
+
+    # route = request.GET["route"]
+
+    context = {
+        "form_selects": {
+            "stop": [4870, 4869, 3007, 6283, 6282],
+            "hour": [x for x in range(6, 24)],
+            "day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            "weather": ["Sunny", "Rainy"],
+        },
+        "time_prediction": str(pred),
+    }
+
     return render(request, 'bus/index.html', context)
-
-
-def num_input(request, testnumber1, num2):
-    return HttpResponse('<h1>' + str(testnumber1) + '</h1>' + '<h1>' + str(num2) + '</h1>')
-
-
-def var_input(request, var=None):
-    print(var)
-    return HttpResponse(request.method + var)
-
-
-def form_test(request):
-    print(request.GET)
-    return render(request, 'bus/index.html')
